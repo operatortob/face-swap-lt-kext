b968e961ab7787ce7f08d93512dceaf1cb3f88e5
Operator <135555424+operatortob@users.noreply.github.com>
2023-06-20 01:47:41 +0300

    Add files via upload

--- roop/precompile_kext.py
+++ roop/precompile_kext.py
@@ -1,11 +1,13 @@
 import subprocess
 
-commands = [
-    #['git', 'clone', 'https://github.com/sczhou/CodeFormer.git', '/content/CodeFormer'],    
-]
+precompile_commands = []
+
+def set_commands(new_commands):
+    global precompile_commands
+    precompile_commands = new_commands
 
 def execute_precompile_commands():
-    for command in commands:
+    for command in precompile_commands:
         subprocess.run(command)
 
 if __name__ == '__main__':
